f07347ce99500b67561ab4c01571f6b2eba54a59
YVES10BOBO <123149216+YVES10BOBO@users.noreply.github.com>
2024-06-08 11:07:16 +0200

    Update stringTransform.js
    fourth commit

--- TASKFORCE-CHALLENGES/Challenges/stringTransform.js
+++ TASKFORCE-CHALLENGES/Challenges/stringTransform.js
@@ -1,22 +1,25 @@
-function transformString(mystring) {
+function transformString(mystring){
     const length = mystring.length;
     let answer = '';
 
-    if (length % 15 === 0) { // Condition when length is divisible by both 3 and 5
+    if(length % 15 === 0) { // condition when length is divisible by both 3 and 5
         answer = mystring.split('').reverse().join('');
-        answer = answer.split('').map(char => char.charCodeAt(0)).join(' ');
-    } else if (length % 3 === 0) { // Condition when length is divisible by 3
-        answer = mystring.split('').reverse().join('');
-    } else if (length % 5 === 0) { // Condition when length is divisible by 5
-        answer = mystring.split('').map(char => char.charCodeAt(0)).join(' ');
-    } else { // Condition when length is not divisible by 3 or 5
+        answer = answer.split('').map(char=> char.charCodeAt(0)).join(' ');
+
+    } else if (length % 3 === 0) { // condition when length is divisible by 3
+        answer  = mystring.split('').reverse().join('');
+
+    } else if (length % 5 === 0) { // condition when length is divisible by 5 
+        answer  = mystring.split('').map(char =>char.charCodeAt(0)).join(' ');
+    }else { // condition when length is not divisible by 3 or 5
+         
         answer = mystring;
     }
 
-    return answer;
+   return answer;
 }
 
-// Test cases
+// test the result cases
 console.log(transformString("Hamburger")); // Expected output: "regrubmaH"
-console.log(transformString("Pizza")); // Expected output: "80 105 122 122 97"
-console.log(transformString("Chocolate Chip Cookie").replace(/ /g, "")); // Expected output: "eikooCpihCetalocohC"
+console.log(transformString("Pizza"));// Expected output: "80 105 122 122 97"
+console.log(transformString("Chocolate Chip Cookie").replace(/ /g, "")); //Expected outputFile: "eikooCpihCetalocohC"
